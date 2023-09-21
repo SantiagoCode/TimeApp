2f4924b09c465819f06456a9c1ee35f7c586d3bf
wayelias1 <wayelias13@gmail.com>
2023-09-20 22:19:41 -0400

    borre carpeta dev

--- src/pages/Time.jsx
+++ src/pages/Time.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Time = () => {
 	return <>
-		
-		<div>Time</div>
+		<div className='time-container'>Time: 
+			<button className='button'></button>
+			<div className='time-info'>
+				<span className='time-lugar'></span>
+				<span className='time-fecha'></span>
+				<span className='time-temperatura'></span>
+			</div>
+			<div className='time-img'>
+				<img src="" alt="" />
+				<img src="" alt="" />
+			</div>
+		</div>
+
 	</>
 	;
 };
