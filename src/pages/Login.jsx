730546bfc2b6ccd5181131da6d5ed0157a298db3
lpalacios1410 <luisarmando20092009@gmail.com>
2023-09-20 21:26:17 -0400

    Agregue el css de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import '../dev/login.css';
 
 const Login = () => {
-	return <div>Login</div>;
+	return <div>LOGINX
+
+		<p>Nombre de la web</p>
+		
+		<h1 className ="loginPage-title">Ingresa en nuestraWeb</h1>
+		
+		<a href='www.facebook.com'>facebook</a>
+		<a href='www.google.com'>google</a>
+		<a href='www.gmail.com'>gmail</a>
+<form>
+
+<input type = "text" name= "username"/>
+
+<input type= "password" name= "password"/>
+
+<buttom type="submit" variant="primary">Login</buttom>
+
+</form>
+
+
+		
+		
+		
+		
+		
+		
+		<div className='GreenBox'>
+
+		</div>
+	
+
+
+	</div>;
 };
 
 export default Login;
